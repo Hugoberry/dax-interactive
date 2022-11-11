c12184b5003f68d20f57de23eb73f0ef67f60689
Igor Cotruta <hugoberry314@gmail.com>
2022-11-11 16:10:30 +0000

    cleanup

--- src/Dax.Interactive/DaxKernel.cs
+++ src/Dax.Interactive/DaxKernel.cs
@@ -1,15 +1,18 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Linq;
+using System.Data;
 using System.Data.OleDb;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Formatting.TabularData;
 using Enumerable = System.Linq.Enumerable;
+using System.CommandLine.Parsing;
+
 
 namespace Dax.Interactive
 {
@@ -18,23 +21,27 @@ namespace Dax.Interactive
         IKernelCommandHandler<SubmitCode>
     {
         private readonly string _connectionString;
-        private IEnumerable<IEnumerable<IEnumerable<(string name, object value)>>> _tables;
-
+        /// <summary>
+        /// The set of query result lists to save for sharing later.
+        /// The key will be the name of the value.
+        /// The value is a list of result sets (multiple if multiple queries are ran as a batch)
+        /// </summary>
+        protected Dictionary<string, IReadOnlyCollection<TabularDataResource>> QueryResults { get; } = new();
+        /// <summary>
+        /// Used to store incoming variables passed in via #!share
+        /// </summary>
+        private readonly Dictionary<string, object> _variables = new(StringComparer.Ordinal);
         public DaxKernel(string name, string connectionString) : base(name, "dax")
         {
             _connectionString = connectionString;
         }
 
-        private OleDbConnection OpenConnection()
-        {
-            return new OleDbConnection(_connectionString);
-        }
 
         public virtual async Task HandleAsync(
             SubmitCode submitCode,
             KernelInvocationContext context)
         {
-            using (var connection = OpenConnection())
+            using (var connection = new OleDbConnection(_connectionString))
             {
                 connection.Open();
 
@@ -43,6 +50,7 @@ namespace Dax.Interactive
 
                     using (var reader = dbCommand.ExecuteReader())
                     {
+                        var results = new List<TabularDataResource>();
                         var values = new object[reader.FieldCount];
                         var names = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToArray();
                         DaxKernelUtils.AliasDuplicateColumnNames(names);
@@ -64,17 +72,19 @@ namespace Dax.Interactive
                         }
 
                         var tabularDataResource = table.ToTabularDataResource();
+                        results.Add(tabularDataResource);
                         var explorer = DataExplorer.CreateDefault(tabularDataResource);
                         context.Display(explorer);
+                        StoreQueryResults(results, submitCode.KernelChooserParseResult);
                     }
                 }
             }
 
         }
 
-    }
+        protected virtual void StoreQueryResults(IReadOnlyCollection<TabularDataResource> results, ParseResult commandKernelChooserParseResult)
+        {
+        }
 
-    public class SqlRow : Dictionary<string, object>
-    {
     }
 }
